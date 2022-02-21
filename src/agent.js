cc64610b995a172564133c995c60a32aa84696a1
Cydiater <cydiater@gmail.com>
2022-02-21 10:43:06 +0800

    fix cookie

--- src/agent.js
+++ src/agent.js
@@ -8,14 +8,19 @@ async function fetchCookie(crendential) {
 	await page.keyboard.type(crendential.username);
 	await page.focus('input[name="password"]');
 	await page.keyboard.type(crendential.password);
+	console.log("Logging in info with crendential");
 	await page.click('input[src="initial/all/images/t_09.gif"]');
 	await page.waitForSelector('a[href="mailto:its@tsinghua.edu.cn"]')
-	await page.click('a[href="/render.userLayoutRootNode.uP"]')
-	await page.waitForSelector('a[href="mailto:its@tsinghua.edu.cn"]')
+	const allText = await page.$eval("*", e => e.innerText);
+	if (allText.includes('进入选课')) {
+		console.log("Logging in succeed, have label 进入选课 in current page");
+		await page.click('a[href="/render.userLayoutRootNode.uP"]')
+		await page.waitForSelector('a[href="mailto:its@tsinghua.edu.cn"]')
+	} 
 	await page.goto('https://zhjw.cic.tsinghua.edu.cn/jxmh_out.do');
 	const cookies = await page.cookies();
 	await browser.close();
-	return cookies[0].name + "=" + cookies[0].value;
+	return cookies.map(cookie => cookie.name + "=" + cookie.value, "").join(";");
 }
 
 export { fetchCookie }
